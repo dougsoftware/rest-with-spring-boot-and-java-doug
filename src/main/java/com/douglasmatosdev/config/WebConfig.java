613952a4639c9a6a8d20eaac1201020e067fd0ca
douglasmatosdev <douglasmatosdev@gmail.com>
2025-05-27 11:19:33 -0300

    Content Negotiation via Header Param

--- src/main/java/com/douglasmatosdev/config/WebConfig.java
+++ src/main/java/com/douglasmatosdev/config/WebConfig.java
@@ -13,12 +13,22 @@ public class WebConfig implements WebMvcConfigurer {
 
         // Via EXTENSION. http://localhost:8080/api/person/v1/2.xml or http://localhost:8080/api/person/v1/2.json deprecated on Spring Boot 2.6
         // Via QUERY PARAM http://localhost:8080/api/person/v1/2?MediaType=xml
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        /**
+         configurer.favorParameter(true)
+         .parameterName("mediaType")
+         .ignoreAcceptHeader(true)
+         .useRegisteredExtensionsOnly(false)
+         .defaultContentType(MediaType.APPLICATION_JSON)
+         .mediaType("json", MediaType.APPLICATION_JSON)
+         .mediaType("xml", MediaType.APPLICATION_XML);
+         */
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
+
+        // Via HEADER PARAM http://localhost:8080/api/person/v1/2
     }
 }
